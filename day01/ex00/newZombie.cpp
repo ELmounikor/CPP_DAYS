319be0d613aad12350c925dee50a24fcec3f60cb
Mounia El koraichi <mel-kora@e2r10p2.1337.ma>
2023-01-09 10:55:04 +0100

    add some missing files in 00

--- day01/ex00/newZombie.cpp
+++ day01/ex00/newZombie.cpp
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mel-kora <mel-kora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/26 22:43:31 by mounikor          #+#    #+#             */
-/*   Updated: 2023/01/09 10:53:38 by mel-kora         ###   ########.fr       */
+/*   Created: 2023/01/09 10:52:48 by mel-kora          #+#    #+#             */
+/*   Updated: 2023/01/09 10:53:07 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Zombie.hpp"
 # include <iostream>
 
-int main(void){
+Zombie* newZombie( std::string name ){
+    
+    Zombie *zamzam;
 
-    randomChump("howa");
-    Zombie* zz = newZombie("hiya");
-    zz->announce();
-    delete zz;
+    zamzam = new Zombie(name);
+    return (zamzam);
 }
--- day01/ex00/randomChump.cpp
+++ day01/ex00/randomChump.cpp
@@ -1,22 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mel-kora <mel-kora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/26 22:43:31 by mounikor          #+#    #+#             */
-/*   Updated: 2023/01/09 10:53:38 by mel-kora         ###   ########.fr       */
+/*   Created: 2023/01/09 10:53:27 by mel-kora          #+#    #+#             */
+/*   Updated: 2023/01/09 10:53:44 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Zombie.hpp"
 # include <iostream>
 
-int main(void){
+void randomChump( std::string name ){
+    
+    Zombie zamzam(name);
 
-    randomChump("howa");
-    Zombie* zz = newZombie("hiya");
-    zz->announce();
-    delete zz;
+    zamzam.announce();
 }
