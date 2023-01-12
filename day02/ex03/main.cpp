ff0c50f7eb9d791024f9597a667ca29be2cff8f0
Mounia El koraichi <mel-kora@e2r11p11.1337.ma>
2023-01-12 10:27:58 +0100

    day 02 done hopefully

--- day02/ex03/main.cpp
+++ day02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-kora <mel-kora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 11:59:42 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/01/12 08:59:04 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/01/12 10:24:56 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@ int main( void )
     Point p(3.5, 3);
     Point op(2, 2);
 
-    std::cout << "triangle abc area =" << Point::area(a, b, c) << "\n" << bsp(a, b, c, p) << std::endl;
-
+    std::cout << "triangle abc area is " << Point::area(a, b, c) << "\nIs that point inside the triangle abc?\n";
+    if(bsp(a, b, c, p))
+        std::cout << "Yes\n";
+    else
+        std::cout << "No\n";
     return 0;
 }
