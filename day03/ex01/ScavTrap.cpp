05653ff673c6524777a153dcbb53c3ee102f7c15
Mounia El koraichi <mel-kora@e1r12p10.1337.ma>
2023-01-13 12:42:42 +0100

    cpp module 03 ex01

--- day03/ex01/ScavTrap.cpp
+++ day03/ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-kora <mel-kora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 10:24:52 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/01/13 12:21:23 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/01/13 12:39:37 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
 {
+	this->setHit_pts(100);
+	this->setHit_pts(50);
+	this->setAttack_dmg(20);
 	std::cout << "ScavTrap " << this->getName() << " is ready\n";
 }
 
--- day03/ex01/main.cpp
+++ day03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-kora <mel-kora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 12:21:15 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/01/13 12:22:10 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/01/13 12:42:32 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,12 @@ int main()
 	ClapTrap p3(p2);
 	
 	p3.setName("khtna2");
-	attaking(p1, p3);
-	p1.setAttack_dmg(5);
-	p3.setAttack_dmg(7);
 	for (int i = 0; i < 5; i++)
 	{
-		attaking(p1, p3);
-		p3.beRepaired(5);
-		attaking(p3, p1);
-		p1.beRepaired(7);
+		attaking(p1, p2);
+		p2.beRepaired(15);
+		attaking(p2, p1);
+		p1.beRepaired(15);
 	}
 	p1.guardGate();
 	return 0;
