9faa10688b2e96cb05dce52c65185009ae7b1208
Mounia El koraichi <mel-kora@e2r10p8.1337.ma>
2023-01-20 15:27:09 +0100

    day04 5

--- day04/ex01/main.cpp
+++ day04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-kora <mel-kora@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 13:09:43 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/01/20 15:14:11 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/01/20 15:27:03 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,33 @@
 
 int main()
 {
-    const Animal* meta = new Animal();
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound(); //will output the cat sound!
-    j->makeSound(); //will output the dog sound!
-    delete i;
-    delete j;
-    delete meta;
+    unsigned int n = 3;
+    Animal *array[n];
+    for (unsigned int i = 0 ; i < n ; i++)
+    {
+        if (i % 2)
+            array[i] = new Cat();
+        else
+            array[i] = new Dog();
+    }
+    for (unsigned int i = 0 ; i < n ; i++)
+        array[i]->makeSound();
+
+    for (unsigned int i = 0 ; i < n ; i++)
+        delete array[i];
+
+    // const Animal* meta = new Animal();
+    // const Animal* j = new Dog();
+    // const Animal* i = new Cat();
+    // std::cout << j->getType() << " " << std::endl;
+    // std::cout << i->getType() << " " << std::endl;
+    // i->makeSound(); //will output the cat sound!
+    // j->makeSound(); //will output the dog sound!
+    // delete i;
+    // delete j;
+    // delete meta;
+
+    
     // Cat *a = new Cat();
     // a->getBrain()->setIdea(0, "hello");
     // Cat *b = new Cat(*a);
