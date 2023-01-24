8cb363d49586369ece83b715ec2091eee03bc182
Mounia El koraichi <mel-kora@e2r11p11.1337.ma>
2023-01-24 15:22:59 +0100

    cpp module 06 ex00

--- day06/ex00/main.cpp
+++ day06/ex00/main.cpp
@@ -6,15 +6,44 @@
 /*   By: mel-kora <mel-kora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 13:16:23 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/01/23 18:14:18 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/01/24 15:22:20 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "header.hpp"
+# include	<iostream>
+# include <limits.h>
+
+void print_char(double value)
+{
+	if (value >= 32 && value <= 127)
+		std::cout << "char: " << static_cast<char>(value) << std::endl;
+	else if (value >= INT_MIN && value <= INT_MAX)
+		std::cout << "char: " << "Non displayable" << std::endl;
+	else
+		std::cout << "char: " << "impossible" << std::endl;
+}
+
+void print_int(double value)
+{
+	if (value >= INT_MIN && value <= INT_MAX)
+		std::cout << "int: " << static_cast<int>(value) << std::endl;
+	else
+		std::cout << "int: " << "impossible" << std::endl;
+}
+
+void print_float_nd_double(double value)
+{
+	std::cout << "float: " << static_cast<float>(value)  << (value == static_cast<int>(value) ? ".0" : "") << 'f' << std::endl;
+	std::cout << "double: " << static_cast<double>(value) << (value == static_cast<int>(value) ? ".0" : "") << std::endl;
+}
 
 int is_numerical(std::string input)
 {
 	unsigned long i = 0;
+	
+	if (input[i] == '-' && i + 1 < input.length() && input[i + 1] >= '0' && input[i + 1] <= '9')
+		i++;
 	while (i < input.length() && input[i] >= '0' && input[i] <= '9')
 		i++;
 	if (i < input.length())
@@ -44,29 +73,18 @@ int is_numerical(std::string input)
 	return (1);
 }
 
-void print_char(std::string input)
+double double_conv(std::string input, int type)
 {
-	if (input.length() == 1)
-		return;
+	if (type == 3 || type == 1)
+		return (std::stod(input));
+	else if (type == 2)
+	{
+		input[input.length() - 1] = 0;
+		return (std::stod(input));
+	}
+	return (static_cast<int>(input[0]));
 }
 
-void print_int(std::string input)
-{
-	if (input.length() == 1)
-		return;
-}
-
-void print_float(std::string input)
-{
-	if (input.length() == 1)
-		return;
-}
-
-void print_double(std::string input)
-{
-	if (input.length() == 1)
-		return;
-}
 int main(int ac, char **av)
 {
 	if (ac == 2)
@@ -77,15 +95,28 @@ int main(int ac, char **av)
 			std::cout << BOLD_Bright_Red << "Nothing to convert\n" << Color_CLEAR;
 			return (1);
 		}
-		std::cout << is_numerical(input) << std::endl;
-		if (is_numerical(input))
+		int type = is_numerical(input);
+		if (input == "-inff" || input == "+inff" || input == "nanf")
+			type = 2;
+		else if (input == "-inf" || input == "+inf" || input == "nan")
+			type = 3;
+		else if (input.length() == 1)
+			type = 4;
+		else if (!type)
 		{
-			std::cout << BOLD_Bright_White << "char: " << (char) std::stoi(input) << std::endl;
-			std::cout << BOLD_Bright_White << "int: " << std::stoi(input) << std::endl;
-			std::cout << BOLD_Bright_White << "float: " << std::stof(input) << std::endl;
-			std::cout << BOLD_Bright_White << "double: " << std::stod(input) << std::endl;
+			std::cout << "char: " << "impossible" << std::endl;
+			std::cout << "int: " << "impossible" << std::endl;
+			std::cout << "float: " << "impossible" << std::endl;
+			std::cout << "double: " << "impossible" << std::endl;
+			return (0);
 		}
+		double value = double_conv(input, type);
+		print_char(value);
+		print_int(value);
+		print_float_nd_double(value);
 	}
 	else
 		std::cout << BOLD_Bright_Red << "Either too little or too much arguments\n" << Color_CLEAR;
+	return 1;
 }
+			
