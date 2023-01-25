ec737ed31bcb3f95be00cefbcadf195e446541d6
Mounia El koraichi <mel-kora@e1r10p11.1337.ma>
2023-01-25 13:36:16 +0100

    cpp module 06 ex01

--- day06/ex01/Data.hpp
+++ day06/ex01/Data.hpp
@@ -6,7 +6,7 @@
 /*   By: mel-kora <mel-kora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 16:43:04 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/01/24 17:58:50 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/01/25 12:43:42 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@
 
 struct Data
 {
-    int a = 5;
-    int b = 18;
-    char *c = "mounia";
-    double d = 45.52;
+	int			a;
+	int			b;
+	char		c;
+	bool		d;
+	std::string	e;
+	float		f;
 };
 
 
--- day06/ex01/main.cpp
+++ day06/ex01/main.cpp
@@ -6,30 +6,45 @@
 /*   By: mel-kora <mel-kora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 16:49:33 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/01/24 18:00:56 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/01/25 13:33:38 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Data.hpp"
+#include "stdlib.h"
 
 uintptr_t serialize(Data* ptr)
 {
-    return(reinterpret_cast<uintptr_t>(ptr));
+	return(reinterpret_cast<uintptr_t>(ptr));
 }
 
 Data* deserialize(uintptr_t raw)
 {
-    return(reinterpret_cast<Data *>(raw));
+	return(reinterpret_cast<Data *>(raw));
 }
 
 int main()
 {
-    Data data;
-    std::cout << sizeof(data) << std::endl;
-    std::cout << data.a << std::endl;
-    std::cout << data.b << std::endl;
-    std::cout << data.c << std::endl;
-    std::cout << data.d << std::endl;
-    uintptr_t  ptr = serialize(&data);
-    
+	uintptr_t  ptr;
+	{
+		Data data;
+	
+		data.a = 5;
+		data.b = 18;
+		data.c = 'M';
+		data.d = 1;
+		data.e = "Mounia";
+		data.f = 1337.42f;
+		ptr = serialize(&data);
+		std::cout << "sizeof(data):" <<  sizeof(data) << std::endl;
+		std::cout << "sizeof(ptr):" << sizeof(ptr) << std::endl;
+	}
+	Data *data_retrieving = deserialize(ptr);
+	std::cout << "sizeof(data retrieving):" << sizeof(*data_retrieving) << std::endl;
+	std::cout << "a = " << data_retrieving->a << std::endl;
+	std::cout << "b = " << data_retrieving->b << std::endl;
+	std::cout << "c = " << data_retrieving->c << std::endl;
+	std::cout << "d = " << data_retrieving->d << std::endl;
+	std::cout << "e = " << data_retrieving->e << std::endl;
+	std::cout << "f = " << data_retrieving->f << std::endl;
 }
