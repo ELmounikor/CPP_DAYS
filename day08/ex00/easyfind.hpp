6db3fcb79cfa5bc517d5342a42295fa626eb9647
Mounia El koraichi <mel-kora@e2r12p12.1337.ma>
2023-01-30 17:07:52 +0100

    CPP Module 08

--- day08/ex00/easyfind.hpp
+++ day08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: mel-kora <mel-kora@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 19:10:25 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/01/27 19:19:25 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/01/30 16:50:20 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,30 @@
 # define _easyfind_hpp
 # include <iostream>
 # include <algorithm>
+# include <array>
+# include <list>
+# include <vector>
+
+class ElementNotFound: public std::exception
+{
+	const char* what() const throw()
+	{
+		return ("\033[1;91mElement Not Found!\033[0m");
+	}
+};
 
 template <class T>
 int &easyfind(T container, int to_find)
 {
-    
+    typename T::iterator i;
+    for (i = container.begin(); i != container.end(); i++)
+    {
+        if (*i == to_find)
+            return(*i);
+    }
+    throw ElementNotFound();
 }
+
+
 
 #endif
--- day08/ex00/main.cpp
+++ day08/ex00/main.cpp
@@ -6,7 +6,26 @@
 /*   By: mel-kora <mel-kora@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 19:10:34 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/01/27 19:10:35 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/01/30 16:24:13 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
+int main()
+{
+	try
+	{
+		std::vector<int> c2(10, 0);
+		c2.push_back(1);
+		c2.push_back(10);
+		c2.push_back(100);
+		c2.push_back(1000);
+		std::cout << ::easyfind(c2, 10) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	return 0;
+}
