0aa7bbd550a9a07b43cde0cc097518cd67c68369
Mounia El koraichi <mel-kora@e2r12p10.1337.ma>
2023-01-31 12:58:10 +0100

    CPP Module 08 ex00

--- day08/ex00/easyfind.hpp
+++ day08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: mel-kora <mel-kora@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 19:10:25 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/01/30 16:50:20 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/01/31 12:58:01 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,18 @@ class ElementNotFound: public std::exception
 template <class T>
 int &easyfind(T container, int to_find)
 {
-    typename T::iterator i;
-    for (i = container.begin(); i != container.end(); i++)
-    {
-        if (*i == to_find)
-            return(*i);
-    }
+    typename T::iterator finding = std::find(container.begin(), container.end(), to_find);
+    if (finding != container.end())
+        return (*finding);
     throw ElementNotFound();
 }
 
-
+template <class T> void print_it(T container)
+{
+    typename T::iterator i;
+	for (i = container.begin() ; i < container.end() ; i++)
+		std::cout << *i << "\t"; 
+	std::cout << std::endl;
+}
 
 #endif
--- day08/ex00/main.cpp
+++ day08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-kora <mel-kora@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 19:10:34 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/01/30 16:24:13 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/01/31 12:56:56 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ int main()
 {
 	try
 	{
-		std::vector<int> c2(10, 0);
-		c2.push_back(1);
-		c2.push_back(10);
-		c2.push_back(100);
-		c2.push_back(1000);
-		std::cout << ::easyfind(c2, 10) << std::endl;
+		std::array<int, 5> c1 = {1, 2, 3, 4, 5};
+		print_it(c1);
+		std::cout << ::easyfind(c1, 3) << std::endl;
+		std::vector<int> c2(7, 0);
+		print_it(c2);
+		std::cout << ::easyfind(c2, 7) << std::endl;
 	}
 	catch(const std::exception& e)
 	{
